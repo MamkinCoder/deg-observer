3799919c917cd0d6a16dec7405c7f82ba8b8c68b
MamkinCoder <yarikbahonov@yahoo.com>
2024-03-16 19:59:26 +0300

    fixed grafana

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,40 +17,40 @@ import {
   TableRow,
 } from "@nextui-org/react";
 
-const FlourishEmbed: React.FC = () => {
-  return (
-    <>
-      <iframe
-        src="https://flo.uri.sh/visualisation/17171317/embed"
-        title="Interactive or visual content"
-        className="flourish-embed-iframe"
-        frameBorder="0"
-        scrolling="no"
-        style={{ width: "100%", height: "600px" }}
-        sandbox="allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation"
-      />
-      <div style={{ width: "100%", marginTop: "4px", textAlign: "right" }}>
-        <a
-          className="flourish-credit"
-          href="https://public.flourish.studio/visualisation/17171317/?utm_source=embed&utm_campaign=visualisation/17171317"
-          target="_top"
-          style={{ textDecoration: "none" }}
-        >
-          <img
-            alt="Made with Flourish"
-            src="https://public.flourish.studio/resources/made_with_flourish.svg"
-            style={{
-              width: "105px",
-              height: "16px",
-              border: "none",
-              margin: "0",
-            }}
-          />
-        </a>
-      </div>
-    </>
-  );
-};
+// const FlourishEmbed: React.FC = () => {
+//   return (
+//     <>
+//       <iframe
+//         src="https://flo.uri.sh/visualisation/17171317/embed"
+//         title="Interactive or visual content"
+//         className="flourish-embed-iframe"
+//         frameBorder="0"
+//         scrolling="no"
+//         style={{ width: "100%", height: "600px" }}
+//         sandbox="allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation"
+//       />
+//       <div style={{ width: "100%", marginTop: "4px", textAlign: "right" }}>
+//         <a
+//           className="flourish-credit"
+//           href="https://public.flourish.studio/visualisation/17171317/?utm_source=embed&utm_campaign=visualisation/17171317"
+//           target="_top"
+//           style={{ textDecoration: "none" }}
+//         >
+//           <img
+//             alt="Made with Flourish"
+//             src="https://public.flourish.studio/resources/made_with_flourish.svg"
+//             style={{
+//               width: "105px",
+//               height: "16px",
+//               border: "none",
+//               margin: "0",
+//             }}
+//           />
+//         </a>
+//       </div>
+//     </>
+//   );
+// };
 
 const HomePage = () => {
   return (
@@ -60,15 +60,14 @@ const HomePage = () => {
       </div>
       <Card className="col-span-1 md:col-span-1 min-h-[50vh]">
         <CardBody>
-          {/* <div className="relative pt-[80%]"> */}
-          <FlourishEmbed />
-
-          {/* <iframe
+          <div className="relative pt-[80%]">
+            {/* <FlourishEmbed /> */}
+            <iframe
               id="contentFrame"
               src="https://losevpeter.ru/grafana/d/edfqkznlpa77ka/deg?orgId=1&kiosk"
               className="absolute inset-0 w-full h-full"
-            ></iframe> */}
-          {/* </div> */}
+            ></iframe>
+          </div>
         </CardBody>
       </Card>
       <Card className="col-span-1 md:col-span-1">
